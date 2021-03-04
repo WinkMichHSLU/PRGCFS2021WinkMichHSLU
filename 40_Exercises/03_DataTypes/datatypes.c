c38967a581801e4ecb29610ed2b2d91e3c8a01d7
Michael Winkler <michael.winkler97@gmx.ch>
2021-03-04 19:39:46 +0100

    add files

---
+++ 40_Exercises/03_DataTypes/datatypes.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+	printf("size of char: %ld byte(s)\n", sizeof(char) );
+	printf("size of unchar: %ld byte(s)\n", sizeof(unsigned char) );
+	printf("size of short: %ld byte(s)\n", sizeof(short) );
+	printf("size of unshort: %ld byte(s)\n", sizeof(unsigned short) );
+	printf("size of int: %ld byte(s)\n", sizeof(int) );
+	printf("size of unint: %ld byte(s)\n", sizeof(unsigned int) );
+	printf("size of float: %ld byte(s)\n", sizeof(float) );
+	
+	
+
+
+	return 0;
+}
