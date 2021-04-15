4b4f75229d550be6b6e352a9ef652535fc098af3
Michael Winkler <michael.winkler97@gmx.ch>
2021-04-15 19:55:42 +0200

    update SW07

---
+++ 40_Exercises/10_pointers/rectangle.h
@@ -0,0 +1,15 @@
+
+struct Point2d {
+	double x;
+	double y;
+};
+
+
+struct Rectangle {
+	struct Point2d bottomLeftCorner;
+	struct Point2d topRightCorner;
+	double area;
+	double perimeter;
+};
+
+double ComputeRectangleArea(struct Rectangle *pRect);
